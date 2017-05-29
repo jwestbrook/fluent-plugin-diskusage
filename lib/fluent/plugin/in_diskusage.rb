4f1de8baa8c7c52821daf706b15b8bea7cd6d86e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-05-29 13:51:13 +0900

    Migrate to use v0.14 API

--- lib/fluent/plugin/in_diskusage.rb
+++ lib/fluent/plugin/in_diskusage.rb
@@ -1,13 +1,16 @@
-require 'fluent/input'
+require 'sys/filesystem'
+require 'fluent/plugin/input'
 
-class Fluent::DiskUsage < Fluent::Input
+class Fluent::Plugin::DiskUsage < Fluent::Plugin::Input
 	Fluent::Plugin.register_input('diskusage',self)
+
+	helpers :timer
 
 	# Define `router` method to support v0.10.57 or earlier
 	unless method_defined?(:router)
 		define_method("router") { Fluent::Engine }
 	end
-	
+
 	config_param :tag,		:string
 	config_param :mountpoint,	:string
 	config_param :label,		:string
@@ -15,19 +18,11 @@ class Fluent::DiskUsage < Fluent::Input
 
 	def configure(conf)
 		super
-		require 'sys/filesystem'
 	end
 
 	def start
 		super
-		@watcher = Thread.new(&method(:run))
-	end
-
-	def run
-		while true
-			output
-		sleep @refresh_interval
-		end
+		timer_execute(:in_diskusage_timer, @refresh_interval, &method(:output))
 	end
 
 	def output
@@ -37,19 +32,17 @@ class Fluent::DiskUsage < Fluent::Input
 		used_bytes = total_bytes - free_bytes
 		used_percent = 0
 		free_percent = 0
-		if total_bytes.nonzero?	
+		if total_bytes.nonzero?
 			used_percent = used_bytes / total_bytes.to_f
 			free_percent = free_bytes / total_bytes.to_f
 		end
 		record = {"label"=>@label,"total_bytes"=>total_bytes,"free_bytes"=>free_bytes,"used_bytes"=>used_bytes,"used_percent"=>used_percent,"free_percent"=>free_percent}
-	
+
 		time = Fluent::Engine.now
 		router.emit(tag,time,record)
 	end
 
 	def shutdown
-		@watcher.terminate
-		@watcher.join
 		super
 	end
 end
